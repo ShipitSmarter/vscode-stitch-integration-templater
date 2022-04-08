5fcb3c34f39093d5b5476df665f3cbb20280923f
Bram van der Horn <bramvanderhorn@shipitsmarter.com>
2022-04-08 17:04:05 +0200

    Added PS scripting functionality

--- src/extension.ts
+++ src/extension.ts
@@ -2,55 +2,76 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-	  vscode.commands.registerCommand('mypanel.start', () => {
-		// Create and show panel
-		const panel = vscode.window.createWebviewPanel(
-		  'mypanel',  // <--- identifier
-		  'Stitch dashboard', // <--- title
-		  vscode.ViewColumn.One,
-		  {}
-		);
-  
-		// And set its HTML content
-		panel.webview.html = getMyWebviewContent(panel.webview, context);   // <--- HTML
-	  })	
+		vscode.commands.registerCommand('mypanel.start', () => {
+			// Create and show panel
+			const panel = vscode.window.createWebviewPanel(
+				'mypanel',  // <--- identifier
+				'Stitch dashboard', // <--- title
+				vscode.ViewColumn.Two,
+					{}
+			);
+
+			// And set its HTML content
+			panel.webview.html = getMyWebviewContent(panel.webview, context);   // <--- HTML
+		})
 	);
-  }
- 
-  function getMyWebviewContent(webview: vscode.Webview, context: any): string { 
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('mypanel.startPowerShellScript1', (uri, files) => {
+			let fileName = '';
+			let filePath = '';
+
+			if(typeof files !== 'undefined' && files.length > 0) {
+				let url = vscode.workspace.asRelativePath(files[0].path);
+				fileName = url.replace(/\\/g, '/').split('/').pop() ?? 'leeg';
+				filePath = url.replace(/\\/g, '/').replace(/\/[^\/]+$/,'');
+				let henk = '';
+
+			} else if(uri) {
+				
+			}
+
+			let terminal = vscode.window.createTerminal('bram');
+			terminal.show();
+			//terminal.sendText('Get-Location');
+			terminal.sendText(`cd ${filePath}`);
+			terminal.sendText(`./${fileName}`);
+
+		})
+	);
+}
+
+function getMyWebviewContent(webview: vscode.Webview, context: any): string {
 	let html: string = ``;
-	
+
 	const myStyle = webview.asWebviewUri(vscode.Uri.joinPath(
-		  context.extensionUri, 'media', 'style.css'));   // <--- 'media' is the folder where the .css file is stored
-	
+		context.extensionUri, 'media', 'style.css'));   // <--- 'media' is the folder where the .css file is stored
+
 	// construct your HTML code
 	html += `
-			<!DOCTYPE html>
-			<html>
-				<head>
-				  <link href="${myStyle}" rel="stylesheet" />   
-				</head>
-				<body>
-				  <div class="main"> 
-				  	<h1>Dashboard</h1>
-					  <div>1</div>
-					  <div>2</div>
-					  <div>3</div>
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<link href="${myStyle}" rel="stylesheet" />   
+			</head>
+			<body>
+				<button class="button-34" role="button">Button 34</button>
+				<div class="main"> 
+					<h1>Dashboard</h1>
+						<div>1</div>
+						<div>2</div>
 					<h1>Scenarios</h1>
 					<div class="scenarios">
-					<h2>From<h2>
-					<h2>To<h2>
-					
-					<h2>ServicesLevel<h2>
-
-					<h2>Other Options
-						<li>
-						</li>
+						<h2>From</h2>
+						<h2>To</h2>
+						<h2>ServicesLevel</h2>
+						<h2>Other Options</h2>
+						<li>tests<li>
 					</div>
-				  </div>
-				</body>
-			 </html>
+				</div>
+			</body>
+		</html>
 	`;
 	// -----------------------
 	return html;
-  }
+}
