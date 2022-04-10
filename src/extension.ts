7631301bc297869304f5295a76118c250929c42d
Bram van der Horn <bramvanderhorn@shipitsmarter.com>
2022-04-10 22:19:58 +0200

    Added input field and extra button to execute use input as PowerShell script

--- src/extension.ts
+++ src/extension.ts
@@ -18,22 +18,37 @@ export function activate(context: vscode.ExtensionContext) {
 			panel.webview.html = getMyWebviewContent(panel.webview, context);
 
 			let terminal: vscode.Terminal;
+			let terminalExists: boolean;
 
 			// Handle messages from the webview
 			panel.webview.onDidReceiveMessage(
 				message => {
-					vscode.window.showErrorMessage(message.text);
+					// check if terminal exists and is still alive
+					terminalExists = (terminal && !(terminal.exitStatus));
 
 					switch (message.command) {
-					case 'startScript':
-						terminal = startScript('','',`Write-Host 'Hello World!'`);
-					case 'executeCommand':
-						if (terminal) {
-							terminal.sendText(message.ScriptCommand);
-						} else {
-							startScript('','',message.ScriptCommand);
-						}
+						case 'startScript':
+							if (!terminalExists) {
+								vscode.window.showErrorMessage(message.text);
+								terminal = startScript('','',`Write-Host 'Hello World!'`);
+							} else {
+								vscode.window.showErrorMessage('Script already started!');
+							}
+							break;
+						case 'executeCommand':
+							vscode.window.showErrorMessage('Executing user input command');
+
+							if (terminalExists) {
+								// if terminal exists and has not exited: re-use
+								terminal.sendText(message.text);
+							} else {
+								// else: open new terminal
+								terminal = startScript('','',message.text);
+							}
+							break;
 					}
+
+					return;
 				},
 				undefined,
 				context.subscriptions
@@ -96,24 +111,30 @@ function getMyWebviewContent(webview: vscode.Webview, context: any): string {
 			<body>
 				<button class="button-34" role="button" onclick="startScript()" id="startScript">Start script</button>
 				<div class="main"> 
-					<h1>Dashboard</h1>
-						<div>1</div>
-						<div>2</div>
-					<h1>Scenarios</h1>
+					<h1>...</h1>
 				</div>
-				<input type="text" maxlength="512" id="ScriptCommand" class="searchField"/>
-				<button class="button-34" role="button" onclick="executeCommand()" id="executeCommand">Execute Command</button>
+				<div>
+					<input type="text" maxlength="512" id="ScriptCommand" class="searchField"/>
+				</div>
+				<div>
+				</div>
+				<div>
+					<button class="button-34" role="button" onclick="executeCommand()" id="executeCommand">Execute Command</button>
+				</div>
+				
 
 				<script>
 					const vscodeApi = acquireVsCodeApi(); 
+					var ScriptCommand = '';
+
 					function startScript(){
 						vscodeApi.postMessage({command: "startScript", text: "Start Selected Script"});
 				  	};
 
-					// function executeCommand () {
-					// 	const ScriptCommand = document.getElementById('ScriptCommand').value
-					// 	vscodeApi.postMessage({command: "executeCommand", text: "Execute command inputted by the user", scriptcommand = ScriptCommand});
-					// };
+					function executeCommand() {
+						ScriptCommand = document.getElementById('ScriptCommand').value;
+						vscodeApi.postMessage({command: "executeCommand", text: ScriptCommand});
+					};
 				</script>
 			</body>
 		</html>
