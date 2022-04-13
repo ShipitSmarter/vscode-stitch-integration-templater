4d9d0e8d2565e8b2c6339cb9c7b235776744c3e2
Bram van der Horn <bramvanderhorn@shipitsmarter.com>
2022-04-13 13:02:42 +0200

    Re-added default Hello World panel

---
+++ src/panels/HelloWorldPanel.ts
@@ -0,0 +1,94 @@
+import * as vscode from "vscode";
+import { getUri } from "../utilities/getUri";
+
+export class HelloWorldPanel {
+  // PROPERTIES
+  public static currentPanel: HelloWorldPanel | undefined;
+  private readonly _panel: vscode.WebviewPanel;
+  private _disposables: vscode.Disposable[] = [];
+
+  // constructor
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+
+    this._panel.webview.html = this._getWebviewContent(this._panel.webview, extensionUri);
+    this._setWebviewMessageListener(this._panel.webview);
+
+    // on dispose
+    this._panel.onDidDispose(this.dispose, null, this._disposables);
+  }
+
+  // METHODS
+  public static render(extensionUri: vscode.Uri) {
+    if (HelloWorldPanel.currentPanel) {
+      HelloWorldPanel.currentPanel._panel.reveal(vscode.ViewColumn.One);
+    } else {
+      const panel = vscode.window.createWebviewPanel("hello-world", "Hello World!", vscode.ViewColumn.Two, {
+        enableScripts: true
+      });
+
+      HelloWorldPanel.currentPanel = new HelloWorldPanel(panel, extensionUri);
+    }
+  }
+
+  private _setWebviewMessageListener(webview: vscode.Webview) {
+    webview.onDidReceiveMessage(
+      (message: any) => {
+        const command = message.command;
+        const text = message.text;
+
+        switch (command) {
+          case "hello":
+            vscode.window.showInformationMessage(text);
+            return;
+        }
+      },
+      undefined,
+      this._disposables
+    );
+  }
+
+  private _getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri) {
+    const toolkitUri = getUri(webview, extensionUri, [
+        "node_modules",
+        "@vscode",
+        "webview-ui-toolkit",
+        "dist",
+        "toolkit.js", // A toolkit.min.js file is also available
+    ]);
+
+    const mainUri = getUri(webview, extensionUri, ["scripts", "hello-world.js"]);
+
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <script type="module" src="${toolkitUri}"></script>
+          <script type="module" src="${mainUri}"></script>
+          <title>Hello World!</title>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+          <vscode-button id="howdy">Howdy!</vscode-button>
+        </body>
+      </html>
+    `;
+  }
+
+  // dispose
+  public dispose() {
+    HelloWorldPanel.currentPanel = undefined;
+
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+}
